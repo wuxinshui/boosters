64d58cc7c968db05b70f15463a9b922a1200773e
wuxinshui <rfg2012@163.com>
2016-11-15 11:02:34 +0800

    装饰模式就是给一个对象增加一些新的功能，而且是动态的，要求装饰对象和被装饰对象实现同一个接口，装饰对象持有被装饰对象的实例

---
+++ Algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/decorator/Sourceable.java
@@ -0,0 +1,12 @@
+package org.wuxinshui.boosters.designPatterns.decorator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: FujiRen
+ * Date: 2016/11/15
+ * Time: 10:55
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Sourceable {
+    public void print();
+}
