780905508ec56de84d6c147f173cb68f9ea87654
rfg2012@163.com <security2012>
2017-02-08 23:38:24 +0800

    //最简洁的单例模式 //无偿地提供了序列化机制，绝对防止多次实例化

---
+++ algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/singleton/SingletonEnum.java
@@ -0,0 +1,17 @@
+package org.wuxinshui.boosters.designPatterns.singleton;
+
+/**
+ * Created by Administrator on 2017/2/8.
+ */
+//最简洁的单例模式
+//无偿地提供了序列化机制，绝对防止多次实例化
+public enum SingletonEnum {
+    INSTANCE;
+
+    public static void main(String[] args) {
+        SingletonEnum singletonEnum = SingletonEnum.INSTANCE;
+        SingletonEnum singletonEnum1 = SingletonEnum.INSTANCE;
+        System.out.println(singletonEnum == singletonEnum1);
+
+    }
+}
