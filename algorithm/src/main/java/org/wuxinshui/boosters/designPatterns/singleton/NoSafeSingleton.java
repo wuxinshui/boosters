43a7097c05f12c9e8d581475d53e8ed75d7ead4e
wuxinshui <rfg2012@163.com>
2017-02-13 11:39:26 +0800

    单例模式-线程不安全的懒汉模式

--- algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/singleton/NoSafeSingleton.java
+++ algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/singleton/NoSafeSingleton.java
@@ -7,20 +7,24 @@ package org.wuxinshui.boosters.designPatterns.singleton;
  * Time: 7:31
  * To change this template use File | Settings | File Templates.
  */
-public class Singleton {
+//线程不安全的懒汉模式
+public class NoSafeSingleton {
     //利用静态变量来记录singleton的唯一实例
-    private static Singleton uniqueInstance;
+    private static NoSafeSingleton uniqueInstance;
 
     /**
      * 构造器私有化，只有singleton内部才可以调用
      * 确保不会被其他代码实例化
      */
-    private Singleton(){
+    private NoSafeSingleton(){
     }
 
-    public static Singleton getInstance(){
+    //线程不安全
+    //第一个线程进来判断 uniqueInstance == null，还没有new 出实例的时候 。
+    // 这个时候第二个线程也进来了，判断的uniqueInstance 也是 null，然后也会 new 出实例的
+    public static NoSafeSingleton getInstance(){
         if (null==uniqueInstance){
-            uniqueInstance=new Singleton();
+            uniqueInstance=new NoSafeSingleton();
         }
         return uniqueInstance;
     }
