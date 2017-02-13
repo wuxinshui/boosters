697a08125bc066eb0cab30d57f95cccef02fbcab
wuxinshui <rfg2012@163.com>
2017-02-13 11:39:07 +0800

    单例模式-饿汉模式

--- algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/singleton/HungrySingleton.java
+++ algorithm/src/main/java/org/wuxinshui/boosters/designPatterns/singleton/HungrySingleton.java
@@ -7,27 +7,28 @@ package org.wuxinshui.boosters.designPatterns.singleton;
  * Time: 7:31
  * To change this template use File | Settings | File Templates.
  */
-public class Singleton1 {
+//饿汉模式
+public class HungrySingleton {
     //利用静态变量来记录singleton的唯一实例
-    private static Singleton1 uniqueInstance=new Singleton1();
+    private final static HungrySingleton uniqueInstance=new HungrySingleton();
 
     /**
      * 构造器私有化，只有singleton内部才可以调用
      * 确保不会被其他代码实例化
      */
-    private Singleton1(){
-        System.out.println("Singleton is create");
+    private HungrySingleton(){
+        System.out.println("HungrySingleton is create");
     }
 
-    public static Singleton1 getInstance(){
+    public static HungrySingleton getInstance(){
         return uniqueInstance;
     }
-
+    //其他角色
     public static void createString(){
-        System.out.println("createString in Singleton");
+        System.out.println("createString in HungrySingleton");
     }
 
     public static void main(String[] args) {
-        Singleton1.createString();
+        HungrySingleton.createString();
     }
 }
